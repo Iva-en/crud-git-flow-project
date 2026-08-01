8e0ddb93b76ab35f0f1b8c3438a3be48a31ebf27
Dev Team <dev@example.com>
2026-08-01 03:03:17 +0000

    feat: agregar formulario de login (POST /api/auth/login)

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const productosRouter = require('./routes/productos');
+const authRouter = require('./routes/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,6 +12,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/productos', productosRouter);
+app.use('/api/auth', authRouter);
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
